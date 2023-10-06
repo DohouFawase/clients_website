9f790879806285684c4f05d4853aa76b181994cd
julesadonsi <adonsijules9@gmail.com>
2023-10-06 13:34:44 +0100

    Fix:payment

--- app/Http/Controllers/web/FrontController.php
+++ app/Http/Controllers/web/FrontController.php
@@ -4,88 +4,114 @@ namespace App\Http\Controllers\web;
 
 use App\Http\Controllers\Controller;
 use App\Models\admin\commerce\Categorie;
+use App\Models\admin\commerce\Commande;
 use App\Models\admin\commerce\Product;
 use App\Models\blog\Post;
 use App\Models\blog\Section;
 use Cart;
+use Darryldecode\Cart\Cart as CartCart;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Session;
 
 class FrontController extends Controller
 {
     //
-    public function boutique(){
-        $products = Product::orderBy('created_at', 'asc')->with('category')->paginate(50);   
-        
+    public function boutique()
+    {
+        $products = Product::orderBy('created_at', 'asc')->with('category')->paginate(50);
 
-      $categories = Categorie::orderBy('created_at', 'asc')->limit(4)->get();
-     
-      
+
+        $categories = Categorie::orderBy('created_at', 'asc')->limit(4)->get();
+
+
         return view("web.boutique", [
             'categories' => $categories,
             'products' => $products
         ]);
     }
 
-    public function blog(){
-        $posts = Post::orderBy('created_at', 'asc')->with('sections')->paginate(50);   
-        
+    public function blog()
+    {
+        $posts = Post::orderBy('created_at', 'asc')->with('sections')->paginate(50);
+
 
         $sections = Section::orderBy('created_at', 'asc')->limit(4)->get();
-        return view("web.blog",[
+        return view("web.blog", [
             'posts' => $posts,
             'sections' => $sections
-        ] );
+        ]);
     }
 
-    public function about(){
+    public function about(Request $request)
+    {
+        $public_key = "dsdsds";
+        $private_key = "sdsds";
+        $secret = "dsds";
+        $kkiapay = new \Kkiapay\Kkiapay(
+            $public_key,
+            $private_key,
+            $secret,
+            $sandbox = true
+        );
+        $paymentResponse = $kkiapay->verifyTransaction($request->get('transaction_id'));
+        if ($paymentResponse->status == "SUCCESS") {
+            dd("Vous avez payez une somme de {$paymentResponse->amount}");
+            //Enregistre les informations de la commande 
+            Commande::create([]);
+            //Envoi un mail de sucess
+            Mail::to("dsdsdsd@gmma")->send();
+            //Envoi une section success to notifier dans la view
+            Session::flash("success", 'Paiment effectuer avec success');
+            CartCart::delete();
+
+        }else {
+            return redirect()->back();
+        }
         return view("web.about");
     }
 
-    public function conatct(){
+    public function conatct()
+    {
         return view("web.conatct");
     }
 
-       
 
-    public function categorie(string $slug,Categorie $categorie){
-       $products = $categorie->products;
-    
-       return view('web.categorie', [
-        "products" =>$products,
-        'categorie'=>$categorie
-       ]);
 
+    public function categorie(string $slug, Categorie $categorie)
+    {
+        $products = $categorie->products;
+
+        return view('web.categorie', [
+            "products" => $products,
+            'categorie' => $categorie
+        ]);
     }
 
-    public function show(string $slug,Product $product){
-           $expecteSlug = $product->getSlug(); 
-        if ($slug !== $expecteSlug ) {
+    public function show(string $slug, Product $product)
+    {
+        $expecteSlug = $product->getSlug();
+        if ($slug !== $expecteSlug) {
             # code...
-            return to_route('web.productdetail', ["slug" => $expecteSlug, "product"=>$product]);
+            return to_route('web.productdetail', ["slug" => $expecteSlug, "product" => $product]);
         }
 
         return view('web.productdetail', [
             "product" => $product
         ]);
-
-        
     }
 
-    public function blogdetail(string $slug, $post){
-        $expecteSlug = $post->getPost(); 
-     if ($slug !== $expecteSlug ) {
-         # code...
-         return to_route('web.blogdetail', ["slug" => $expecteSlug, "product"=>$post]);
-     }
-     $posts = Post::orderBy('created_at', 'asc')->limit(2)->get();
-     return view('web.blogdetail', [
-         "post" => $post,
-         "posts" => $posts
-     ]);
-
-     
- }
-
-
-   
+    public function blogdetail(string $slug, $post)
+    {
+        $expecteSlug = $post->getPost();
+        if ($slug !== $expecteSlug) {
+            # code...
+            return to_route('web.blogdetail', ["slug" => $expecteSlug, "product" => $post]);
+        }
+        $posts = Post::orderBy('created_at', 'asc')->limit(2)->get();
+        return view('web.blogdetail', [
+            "post" => $post,
+            "posts" => $posts
+        ]);
+    }
 }
--- resources/views/layouts/custum.blade.php
+++ resources/views/layouts/custum.blade.php
@@ -1,44 +1,29 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="cart-update-url" content="{{ route('cart.cartUpdate') }}">
-    <meta name="csrf-token" content="{{ csrf_token() }}">  
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title') | McBonaSport</title>
     @include('components.web.heade')
     @vite(['resources/css/app.css', 'resources/js/app.js'])
-
 </head>
 <body>
-    
-        <div>
-            @include("components.web.nav")
 
-            @yield("content")
+    <div>
+        @include('components.web.nav')
 
-            @include("components.web.footer")
+        @yield('content')
 
-        </div>
+        @include('components.web.footer')
 
+    </div>
 
-
-
-        
-
-    @include("components.web.scripts")
+    @include('components.web.scripts')
     <script src="https://cdn.kkiapay.me/k.js"></script>
-    <script amount="5000" 
-        callback=""
-        data=""
-        url="<url-vers-votre-logo>"
-        position="center" 
-        theme="#0095ff"
-        sandbox="true"
-        key="8f59a0e0f8a911eca56ad905c440058f"
-        src="https://cdn.kkiapay.me/k.js">
-    </script>
-    
 </body>
+
 </html>
