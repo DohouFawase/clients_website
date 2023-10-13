610f256f4e9f08d5e7e7d3d4f707ff7b0ae12ad0
Ondef <dohfawaz90@gmail.com>
2023-10-13 15:58:45 +0100

    wip

--- resources/views/components/web/nav.blade.php
+++ resources/views/components/web/nav.blade.php
@@ -73,19 +73,19 @@
   <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
     <ul class="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
       <li>
-        <a href="{{"/"}}" class=" {{ Route::is('/') ? 'text-primary rounded bg-black' : ''}}   block py-2 pl-3 pr-4 text-2xl font-semibold rounded  md:p-0 m" >Acceuil</a>
+        <a href="/" class=" {{ request()->is('/') ? 'text-blue-600':'' }}    block py-2 pl-3 pr-4 text-2xl font-semibold rounded  md:p-0 m" >Acceuil</a>
       </li>
       <li>
-        <a href="{{ route('boutique') }}" class="{{request()->is('boutique') ? 'active' : ''}}  block font-semibold py-2 pl-3 text-2xl pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700" >Boutique</a>
+        <a href="{{ route('boutique') }}" class="{{ request()->is('boutique') ? 'text-blue-600':'' }}  block font-semibold py-2 pl-3 text-2xl pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700" >Boutique</a>
       </li>
       <li>
-        <a href="{{ route('about') }}" class="  {{request()->is('about') ? 'active' : ''}} block py-2 font-semibold text-2xl pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">A propos</a>
+        <a href="{{ route('about') }}" class="  {{request()->is('about') ? 'text-blue-600' : ''}} block py-2 font-semibold text-2xl pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">A propos</a>
       </li>
       <li>
-        <a href="{{ route('blog') }}" class="  ablock py-2 font-semibold text-2xl pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Blog</a>
+        <a href="{{ route('blog') }}" class=" {{request()->is('blog') ? 'text-blue-600' : ''}} ablock py-2 font-semibold text-2xl pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Blog</a>
       </li>
       <li>
-          <a href="{{ route('conatct') }}" class="block text-2xl font-semibold py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Contact</a>
+          <a href="{{ route('conatct') }}" class="{{request()->is('contact') ? 'text-blue-600' : ''}} block text-2xl font-semibold py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Contact</a>
         </li>
     </ul>
   </div>
